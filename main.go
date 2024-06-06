bcac5a21f5d4088c4376ac034a34f4183ae5a361
G <spamargsr@gmail.com>
2024-06-05 21:38:32 -0300

    make parallel

--- main.go
+++ main.go
@@ -22,25 +22,52 @@ func main() {
 		return
 	}
 
-	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
+	messagesChannel := make(chan messageEnter)
 
-		if m.Content == "l" || m.Content == "L" {
-			s.ChannelMessageSend(m.ChannelID, ".__ ")
-			s.ChannelMessageSend(m.ChannelID, "|  |")
-			s.ChannelMessageSend(m.ChannelID, "|  | ")
-			s.ChannelMessageSend(m.ChannelID, "|  |__")
-			s.ChannelMessageSend(m.ChannelID, "|____/")
-			return
-		}
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		messagesChannel <- messageEnter{s, m}
 	})
 
 	session.Open()
 	defer session.Close()
 
-	endServer := make(chan os.Signal, 1)
-	signal.Notify(endServer, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-endServer
+	loopHandleMessages(messagesChannel)
+
+}
+
+type messageEnter struct {
+	session *discordgo.Session
+	message *discordgo.MessageCreate
+}
+
+func handleMessage(m messageEnter) {
+	message := m.message
+	session := m.session
+
+	if message.Author.ID == session.State.User.ID {
+		return
+	}
+
+	if message.Content == "l" || message.Content == "L" {
+		session.ChannelMessageSend(message.ChannelID, "⠂⠈⠈⢹⠉⠀⠁⠀⠁⠀")
+		session.ChannelMessageSend(message.ChannelID, "⠀⠐⠀⢸⠀⠀⠁⡀⠁")
+		session.ChannelMessageSend(message.ChannelID, "⠐⠀⢈⣸⣀⣈⣠⡇⠀")
+
+		return
+	}
+}
+
+func loopHandleMessages(c chan messageEnter) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	for {
+		select {
+		case message := <-c:
+			handleMessage(message)
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
 }
